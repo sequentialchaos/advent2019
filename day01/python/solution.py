d75c8362d637ae2d4331e2c769e91936e65c50c6
Winston Smith <wsbraves@gmail.com>
2019-12-06 02:08:53 -0500

    complete day 01 in python (fixes #51)

---
+++ day01/python/solution.py
@@ -0,0 +1,17 @@
+
+with open("input.txt") as f:
+  input01 = list(map(int, f.readlines()))
+
+# PART 1
+def fuel(mass):
+  return int(mass / 3) - 2
+
+print(sum(map(fuel, input01)))
+
+# PART 2
+def fuelfuel(mass, total=0):
+  if fuel(mass) <= 0:
+    return total
+  return total + fuel(mass) + fuelfuel(fuel(mass))
+
+print(sum(map(fuelfuel, input01)))
